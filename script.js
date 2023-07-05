199a5043a9f94597b7643d5293ea7518b77e256f
thugperfect <meenamani9444@gmail.com>
2023-07-05 11:47:19 +0000

    added load more...

--- script.js
+++ script.js
@@ -63,10 +63,3 @@ form.addEventListener("submit",(event)=>{
 load.addEventListener("click",()=>{
     search()
 })
-var elements = document.getElementsByClassName("imgbox");
-function four() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "25%";  // IE10
-    elements[i].style.flex = "25%";
-  }
-}
