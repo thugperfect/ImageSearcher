ceea055ae5eb35ef59d2f4706e990d751465e61a
thugperfect <meenamani9444@gmail.com>
2023-07-05 11:41:04 +0000

    added grid layout

--- script.js
+++ script.js
@@ -63,3 +63,10 @@ form.addEventListener("submit",(event)=>{
 load.addEventListener("click",()=>{
     search()
 })
+var elements = document.getElementsByClassName("imgbox");
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
